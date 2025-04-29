1751f3f4fc2ad2020bdb8f33eff2ff2e2660554b
Niff1er <niff1er.3oy@gmail.com>
2025-04-29 16:16:04 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>index</title>
+        <link rel="stylesheet" href="style.css">
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <script src="https://kit.fontawesome.com/cefac98de9.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <?php include('sidebar.php');?>
+        <div class="main-content">
+            <?php include('dashboard.php');?>
+        </div>
+    </body>
+</html>
---
+++ sidebar.php
@@ -0,0 +1,26 @@
+<div class="sidebar">
+     <div class="head_side">
+          <span id="icon">
+               <i class="fa-solid fa-bolt"></i>
+          </span>
+          <span>Energy</span>
+     </div>
+     <a href="#" class="nav-link active">
+          <span id="icon"> 
+               <i class="fa-solid fa-gauge-high"></i>
+          </span>
+          <span>Dashboard</span>
+     </a>
+     <a href="#" class="nav-link">
+          <span id="icon">
+               <i class="fa-solid fa-bolt"></i>
+          </span>
+          <span>Energy Management</span>
+     </a>
+     <div class="foot_side">
+          <span id="icon">
+               <i class="fa-solid fa-right-from-bracket"></i>
+          </span>
+          <span>Log out</span>
+     </div>
+</div>
